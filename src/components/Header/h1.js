8ebd02f72fa852ca4ae0b1264f7dce8dc92d2f1c
FrancoStorero <franco_storero@hotmail.com>
2021-11-15 19:50:52 -0300

    act

--- src/components/Header/h1.js
+++ src/components/Header/h1.js
@@ -4,7 +4,7 @@ import "../../Assets/styles/_header-styles.scss";
 export default function H1_() {
   return (
     <Container>
-      <Col xl={6} lg={9} sm={8} className="p-0">
+      <Col xl={7} lg={9} sm={8} className="p-0">
         <h1 className="my-h1 text-wrap">
           Sed ut perspiciatis unde omnis iste natus
         </h1>
